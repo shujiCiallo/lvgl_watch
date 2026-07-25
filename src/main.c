d616389ab7561d3147e6a30575ada831cb24832c
shuji <zjk465396750@qq.com>
2026-07-25 14:32:42 +0800

    修改title控件位置,为tools_panel填充了button

--- src/main.c
+++ src/main.c
@@ -63,7 +63,7 @@ int main(int argc, char **argv)
   lv_init();
 
   /*Initialize the HAL (display, input devices, tick) for LVGL*/
-  sdl_hal_init(280, 400);
+  sdl_hal_init(320, 480);
 
   ui_init();
 
--- src/ui/screen/screen_tools.c
+++ src/ui/screen/screen_tools.c
@@ -6,6 +6,7 @@
 static lv_obj_t *s_tools_panel = NULL;
 
 static void title_create(lv_obj_t* parent);
+static void tools_create(lv_obj_t *parent);
 
 void screen_tools_create(lv_obj_t *parent)
 {
@@ -18,21 +19,64 @@ void screen_tools_create(lv_obj_t *parent)
         LV_GRID_TEMPLATE_LAST};
     lv_obj_set_layout(s_tools_panel, LV_LAYOUT_GRID);
     lv_obj_set_grid_dsc_array(s_tools_panel, col_dsc, row_dsc);
-    lv_obj_set_style_pad_row(s_tools_panel, 5, 0);
-    lv_obj_set_style_pad_column(s_tools_panel, 5, 0);
-
+    lv_obj_set_style_pad_row(s_tools_panel, 8, 0);
+    lv_obj_set_style_pad_column(s_tools_panel, 8, 0);
+    lv_obj_set_style_pad_all(parent, 10, 0);
     title_create(s_tools_panel);
+    tools_create(s_tools_panel);
 }
+
+static lv_style_t title_style;
 
 static void title_create(lv_obj_t *parent)
 {
+    lv_style_init(&title_style);
+    lv_style_set_bg_opa(&title_style, LV_OPA_COVER);
+    lv_style_set_bg_color(&title_style, lv_color_hex(0x1234f234));
+    lv_style_set_text_font(&title_style, &lv_font_montserrat_36);
+
     lv_obj_t *bt_label = lv_label_create(parent);
     lv_label_set_text(bt_label, LV_SYMBOL_BLUETOOTH);
-    lv_obj_set_grid_cell(bt_label, LV_GRID_ALIGN_STRETCH,
-        0, 1, LV_GRID_ALIGN_STRETCH, 0, 1);
+    lv_obj_add_style(bt_label, &title_style, 0);
+    lv_obj_set_grid_cell(bt_label, LV_GRID_ALIGN_START,
+        0, 1, LV_GRID_ALIGN_CENTER, 0, 1);
+
+    lv_obj_t *bat_label = lv_label_create(parent);
+    lv_obj_add_style(bat_label, &title_style, 0);
+    lv_label_set_text(bat_label, LV_SYMBOL_BATTERY_1);
+    lv_obj_set_grid_cell(bat_label, LV_GRID_ALIGN_END,
+        0, 1,LV_GRID_ALIGN_STRETCH , 0, 1);
 
     lv_obj_t *time_label = lv_label_create(parent);
+    lv_obj_add_style(time_label, &title_style, 0);
     lv_label_set_text(time_label, time_buf);
     lv_obj_set_grid_cell(time_label, LV_GRID_ALIGN_END,
         1, 1, LV_GRID_ALIGN_STRETCH, 0, 1);
+
+
+}
+
+static void tools_create(lv_obj_t *parent)
+{
+
+    lv_obj_t *SD_crad = lv_btn_create(parent);
+    lv_obj_t *USB = lv_btn_create(parent);
+    lv_obj_t *gear = lv_btn_create(parent);
+    lv_obj_t *trash = lv_btn_create(parent);
+    lv_obj_t *drive = lv_btn_create(parent);
+    lv_obj_t *folder = lv_btn_create(parent);
+
+    lv_obj_t *tools_buf[] = {
+        SD_crad, USB, gear, trash, drive, folder
+    };
+
+    size_t num = sizeof(tools_buf) / sizeof(tools_buf[0]);
+    for (size_t i = 0; i < num; i++) {
+        int col = i % 2;
+        int row = i / 2;
+        lv_obj_set_grid_cell(tools_buf[i], LV_GRID_ALIGN_STRETCH,
+                            col, 1, LV_GRID_ALIGN_STRETCH,
+                            row + 1, 1);
+    }
+
 }

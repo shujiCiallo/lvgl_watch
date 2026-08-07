6f53bea1c250cc315dbf6d25fc9a264ea7141117
shuji <zjk465396750@qq.com>
2026-08-07 15:26:57 +0800

    实现crad点击事件框架

--- src/ui/screen/screen_card.c
+++ src/ui/screen/screen_card.c
@@ -10,6 +10,15 @@ typedef struct label_data {
     lv_obj_t *line2;   /* 数值行 */
 } label_data_t;
 
+/* 单个圆环的创建配置 */
+typedef struct artivity_rings {
+    lv_obj_t *arc;         /* arc 控件 */
+    int size;              /* 直径 */
+    int width;             /* 弧线宽度 */
+    uint32_t max_value;    /* 满量程值 */
+    lv_color_t color;      /* 指示条颜色 */
+} artivity_rings_t;
+
 /* 三个活动圆环各自订阅的数据源 */
 static lv_subject_t *data[] = {
     &g_calorie_subject,
@@ -19,10 +28,14 @@ static lv_subject_t *data[] = {
 /* 模块内部构建函数 */
 static void title_create(screen_card_t *self, lv_obj_t *parent);
 static void card_create(screen_card_t *self, lv_obj_t *parent);
-static lv_obj_t *artivity_rings_create(screen_card_t *self, lv_obj_t *parent);
+
+static lv_obj_t *artivity_rings_create(screen_card_t *self, lv_obj_t *parent,
+    artivity_rings_t *configs);
 static lv_obj_t *sport_card_create(screen_card_t *self, lv_obj_t *parent);
 static lv_obj_t *label_data_create(lv_obj_t *parent);
+
 static void arc_boserver_cb(lv_observer_t *obs, lv_subject_t *sub);
+static void sport_btn_click_cb(lv_event_t *e);
 
 /* 创建卡片屏幕:标题栏 + 运动数据卡片 */
 void screen_card_create(screen_card_t *self, lv_obj_t *parent)
@@ -68,7 +81,7 @@ static void title_create(screen_card_t *self, lv_obj_t *parent)
     }
 }
 
-/* 卡片区入口:目前只有运动数据卡 */
+/* 卡片区入口 */
 static void card_create(screen_card_t *self, lv_obj_t *parent)
 {
     sport_card_create(self, parent);
@@ -77,21 +90,31 @@ static void card_create(screen_card_t *self, lv_obj_t *parent)
 /* 运动数据卡片:活动圆环 + 下方数值区 */
 static lv_obj_t *sport_card_create(screen_card_t *self, lv_obj_t *parent)
 {
-    lv_obj_t *sport_card = lv_btn_create(parent);
+    lv_obj_t *sport_card = lv_obj_create(parent);
     lv_obj_set_style_bg_color(sport_card, COLOR_SURFACE, 0);
     lv_obj_set_flex_flow(sport_card, LV_FLEX_FLOW_COLUMN);
     lv_obj_set_flex_align(sport_card, LV_FLEX_ALIGN_START,
         LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
     lv_coord_t scr_h = lv_disp_get_ver_res(NULL);
     lv_obj_set_size(sport_card, lv_pct(100), (scr_h * 45 / 100));
+    lv_obj_add_event_cb(sport_card, sport_btn_click_cb,
+        LV_EVENT_PRESSED, self);
     {
-        lv_obj_t *meter = lv_obj_create(sport_card);
+        lv_obj_t *meter = lv_btn_create(sport_card);
         lv_obj_clear_flag(meter, LV_OBJ_FLAG_SCROLLABLE);
         lv_obj_remove_style_all(meter);
+        lv_obj_add_flag(meter, LV_OBJ_FLAG_EVENT_BUBBLE);
         lv_obj_set_flex_grow(meter, 1);
-        artivity_rings_create(self, meter);
+        artivity_rings_t configs[] = {
+            {NULL, 60, 6, 400, COLOR_ERROR},
+            {NULL, 90, 6, 6000, COLOR_PRIMARY_DARK},
+            {NULL, 120, 6, 1200, COLOR_SECONDARY}
+        };
+        artivity_rings_create(self, meter, configs);
 
         lv_obj_t *info = lv_obj_create(sport_card);
+        lv_obj_remove_style_all(info);
+        lv_obj_add_flag(info, LV_OBJ_FLAG_EVENT_BUBBLE);
         lv_obj_set_flex_grow(info, 1);
         lv_obj_set_width(info, lv_pct(100));
         lv_obj_set_flex_flow(info, LV_FLEX_FLOW_ROW);
@@ -106,29 +129,17 @@ static lv_obj_t *sport_card_create(screen_card_t *self, lv_obj_t *parent)
     return sport_card;
 }
 
+
 /* 活动圆环:三个同心 arc,各自订阅一个数据源 */
-static lv_obj_t *artivity_rings_create(screen_card_t *self, lv_obj_t *parent)
+static lv_obj_t *artivity_rings_create(screen_card_t *self, lv_obj_t *parent,
+     artivity_rings_t *configs)
 {
-    /* 单个圆环的创建配置 */
-    typedef struct artivity_rings {
-        lv_obj_t *arc;         /* arc 控件 */
-        int size;              /* 直径 */
-        int width;             /* 弧线宽度 */
-        uint32_t max_value;    /* 满量程值 */
-        lv_color_t color;      /* 指示条颜色 */
-    } artivity_rings_t;
-
-    artivity_rings_t configs[] = {
-        {NULL, 60, 6, 400, COLOR_ERROR},
-        {NULL, 90, 6, 6000, COLOR_PRIMARY_DARK},
-        {NULL, 120, 6, 1200, COLOR_SECONDARY}
-    };
-
     for (size_t i = 0; i < 3; i++) {
         configs[i].arc = lv_arc_create(parent);
 
         lv_obj_clear_flag(configs[i].arc, LV_OBJ_FLAG_CLICKABLE);
         lv_obj_clear_flag(configs[i].arc, LV_OBJ_FLAG_CLICK_FOCUSABLE);
+        lv_obj_set_event_bubble(configs[i].arc, true);
         lv_obj_set_size(configs[i].arc, configs[i].size, configs[i].size);
         lv_obj_align(configs[i].arc, LV_ALIGN_BOTTOM_MID,
             0, configs[i].size / 2);
@@ -147,6 +158,7 @@ static lv_obj_t *artivity_rings_create(screen_card_t *self, lv_obj_t *parent)
         /* 订阅数据变化,订阅句柄存到结构体便于统一移除 */
         self->arc_obs[i] = lv_subject_add_observer(data[i],
             arc_boserver_cb, configs[i].arc);
+
     }
     return parent;
 }
@@ -158,13 +170,14 @@ static lv_obj_t *label_data_create(lv_obj_t *parent)
     size_t num = sizeof(label) / sizeof(label[0]);
     char *title[] = {"calorie", "steps", "times"};
     for (size_t i = 0; i < num; i++) {
-        label[i].parent = lv_obj_create(parent);
+        label[i].parent = lv_btn_create(parent);
         lv_obj_remove_style_all(label[i].parent);
         lv_obj_set_flex_grow(label[i].parent, 1);
         lv_obj_set_height(label[i].parent, lv_pct(100));
         lv_obj_set_flex_flow(label[i].parent, LV_FLEX_FLOW_COLUMN);
         lv_obj_set_flex_align(label[i].parent, LV_FLEX_ALIGN_CENTER,
             LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_START);
+        lv_obj_set_event_bubble(label[i].parent, true);
 
         /* 标题行 */
         label[i].line1 = lv_label_create(label[i].parent);
@@ -174,6 +187,7 @@ static lv_obj_t *label_data_create(lv_obj_t *parent)
         lv_obj_set_width(label[i].line1, lv_pct(100));
         lv_obj_set_style_text_align(label[i].line1, LV_TEXT_ALIGN_CENTER, 0);
         lv_label_set_text(label[i].line1, title[i]);
+        lv_obj_set_event_bubble(label[i].line1, true);
 
         /* 数值行:绑定 subject,数据更新自动刷新 */
         label[i].line2 = lv_label_create(label[i].parent);
@@ -183,6 +197,7 @@ static lv_obj_t *label_data_create(lv_obj_t *parent)
         lv_obj_set_width(label[i].line2, lv_pct(100));
         lv_obj_set_style_text_align(label[i].line2, LV_TEXT_ALIGN_CENTER, 0);
         lv_label_bind_text(label[i].line2, data[i], "%d");
+        lv_obj_set_event_bubble(label[i].line2, true);
     }
     return parent;
 }
@@ -193,4 +208,17 @@ static void arc_boserver_cb(lv_observer_t *obs, lv_subject_t *sub)
     lv_obj_t *arc = lv_observer_get_user_data(obs);
     int16_t value = lv_subject_get_int(sub);
     lv_arc_set_value(arc, value);
+}
+
+/*sport按键回调*/
+static void sport_btn_click_cb(lv_event_t *e)
+{
+    lv_obj_t *old_scr = lv_scr_act();
+    screen_card_t *self = lv_event_get_user_data(e);
+    lv_obj_add_flag(old_scr, LV_OBJ_FLAG_HIDDEN);
+
+    lv_obj_t *new = lv_obj_create(NULL);
+    lv_obj_t *btn = lv_btn_create(new);
+
+    lv_scr_load(new);
 }
